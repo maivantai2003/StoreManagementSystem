b9df4093b2de92ca79106f764dee42e03b5495be
vantai <vantai08122003@gmail.com>
2023-10-10 14:23:58 +0700

    Add New Project

--- SportStoreManager/GUI/FormHome.Designer.cs
+++ SportStoreManager/GUI/FormHome.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GUI
 {
-    partial class Form1
+    partial class FormHome
     {
         /// <summary>
         /// Required designer variable.
--- SportStoreManager/GUI/FormHome.cs
+++ SportStoreManager/GUI/FormHome.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class FormHome : Form
     {
-        public Form1()
+        public FormHome()
         {
             InitializeComponent();
         }
